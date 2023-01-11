d87a429bee50f41f742d2d310a728300d6fc3ad4
murylo <murylo.diasolirub@gmail.com>
2023-01-11 19:13:36 -0300

    criando mais funções de botão

--- app/src/main/java/com/example/projetoorientacaoobjetos/CadastroPessoa.java
+++ app/src/main/java/com/example/projetoorientacaoobjetos/CadastroPessoa.java
@@ -3,6 +3,7 @@ package com.example.projetoorientacaoobjetos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroPessoa extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class CadastroPessoa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pessoa2);
+    }
+
+    public void cadastraPessoa(View view) {
     }
 }
--- app/src/main/java/com/example/projetoorientacaoobjetos/Colheita.java
+++ app/src/main/java/com/example/projetoorientacaoobjetos/Colheita.java
@@ -3,6 +3,7 @@ package com.example.projetoorientacaoobjetos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Colheita extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Colheita extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colheita);
+    }
+
+    public void AdicionaNovosSacos(View view) {
     }
 }
