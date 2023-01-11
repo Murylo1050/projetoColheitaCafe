1e7877bc441cdf91ca6242fbd75d237b485135f2
murylo <murylo.diasolirub@gmail.com>
2023-01-11 18:19:48 -0300

    colocando as mudanças de tela

---
+++ app/src/main/java/com/example/projetoorientacaoobjetos/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.projetoorientacaoobjetos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void moveToCadastro(View view) {
+        Intent in = new Intent(this,Cadastro.class);
+        startActivity(in);
+    }
+
+    public void moveToColheita(View view) {
+        Intent in = new Intent(this,Colheita.class);
+        startActivity(in);
+    }
+}
