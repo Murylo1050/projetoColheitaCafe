43fe4820edc2eb43acb0d095e384c31e2d207866
murylo <murylo.diasolirub@gmail.com>
2023-01-12 17:57:26 -0300

    add funções de criação de objetos

---
+++ app/src/main/java/com/example/projetoorientacaoobjetos/OBJETOS/Grupo.java
@@ -0,0 +1,13 @@
+package com.example.projetoorientacaoobjetos.OBJETOS;
+
+public class Grupo {
+
+    public Grupo(String nome){
+        this.nome = nome;
+
+    }
+    private String nome;
+
+
+
+}
