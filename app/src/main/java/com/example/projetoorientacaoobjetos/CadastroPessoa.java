728a577765d134aeca3f5ba7f1bbd90d1c0a6435
Murylo Dias <114259784+Murylo1050@users.noreply.github.com>
2023-01-12 18:06:10 -0300

    Revert "add funções de criação de objetos"

--- app/src/main/java/com/example/projetoorientacaoobjetos/CadastroPessoa.java
+++ app/src/main/java/com/example/projetoorientacaoobjetos/CadastroPessoa.java
@@ -1,33 +1,18 @@
 package com.example.projetoorientacaoobjetos;
 
-import static com.example.projetoorientacaoobjetos.CadastroGrupo.getGrupos;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.example.projetoorientacaoobjetos.OBJETOS.Grupo;
-
-import java.util.List;
 
 public class CadastroPessoa extends AppCompatActivity {
-    ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pessoa2);
-        lista = (ListView)findViewById(R.id.listViewCadastraPessoa);
-
-        ArrayAdapter<Grupo> arrayAdapter = new ArrayAdapter<Grupo>(this, android.R.layout.simple_list_item_1,getGrupos());
-
-        lista.setAdapter(arrayAdapter);
     }
 
     public void cadastraPessoa(View view) {
-
     }
 }
