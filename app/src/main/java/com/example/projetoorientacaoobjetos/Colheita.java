6b0c55488fd077ed101cc9488a4401bdac7d5367
murylo <murylo.diasolirub@gmail.com>
2023-01-14 20:34:33 -0300

    backend da colheita

--- app/src/main/java/com/example/projetoorientacaoobjetos/Colheita.java
+++ app/src/main/java/com/example/projetoorientacaoobjetos/Colheita.java
@@ -1,6 +1,7 @@
 package com.example.projetoorientacaoobjetos;
 
 import static com.example.projetoorientacaoobjetos.CadastroGrupo.getGrupos;
+import static com.example.projetoorientacaoobjetos.CadastroPessoaReal.getPessoas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,15 +9,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.projetoorientacaoobjetos.OBJETOS.Grupo;
+import com.example.projetoorientacaoobjetos.OBJETOS.Pessoa;
 
 import java.util.ArrayList;
 
 public class Colheita extends AppCompatActivity {
-    private Spinner lista;
+    private Spinner listaGrupo;
+    private EditText idPessoa;
+    String[] equipes;
+
     private ArrayList<String> equipe = new ArrayList<>();
     private ArrayList<Grupo> grupos = new ArrayList<>();
 
@@ -25,45 +31,38 @@ public class Colheita extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colheita);
-        lista = (Spinner) findViewById(R.id.spinnerColheitaGrupo);
+        listaGrupo = (Spinner) findViewById(R.id.spinnerColheitaGrupo);
+        idPessoa = (EditText)findViewById(R.id.et_nome_pessoa_colheita);
+
 
         grupos.addAll(getGrupos());
 
-        System.out.println("Ola mundo");
-        //System.out.println(getGrupos().get(1).getNome());
-
-
         for (Grupo grupo: grupos) {
-            //System.out.println(grupo.getNome());
-            equipe.add(grupo.getNome());
+            equipe.add(grupo.getNomeGrupo());
         }
 
-        String[] equipes = equipe.toArray(new String[0]);
-
-        for (String sexo: equipes) {
-            System.out.println(sexo);
-        }
-
-
-
+        equipes = equipe.toArray(new String[0]);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, equipes);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        lista.setAdapter(arrayAdapter);
+        listaGrupo.setAdapter(arrayAdapter);
 
-        lista.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        listaGrupo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
                 String value=adapterView.getItemAtPosition(position).toString();
-                Toast.makeText( Colheita.this, equipes[position],Toast.LENGTH_SHORT).show();
-            }
 
+            }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
-    }
 
+
+    }
+    private int cont=0;
     public void AdicionaNovosSacos(View view) {
+        while(!(grupos.get(listaGrupo.getSelectedItemPosition()).getPessoasDaEquipe().get(cont).getId() == Integer.parseInt(idPessoa.getText());
     }
 }
--- app/src/main/java/com/example/projetoorientacaoobjetos/OBJETOS/Pessoa.java
+++ app/src/main/java/com/example/projetoorientacaoobjetos/OBJETOS/Pessoa.java
@@ -1,14 +1,32 @@
 package com.example.projetoorientacaoobjetos.OBJETOS;
 
 public class Pessoa {
+    private int contId = 0;
 
     public Pessoa(String nome, Grupo grupo){
+        contId+=1;
         this.nome = nome;
         this.grupo = grupo;
+        this.id = contId;
+        this.qtdSacos=0;
     }
 
     private String nome;
     private Grupo grupo;
+    private int id;
+    private int qtdSacos;
 
-    public String getNome() { return nome; }
+    public int getQtdSacos() {
+        return qtdSacos;
+    }
+
+    public void setQtdSacos(int qtdSacos) {
+        this.qtdSacos = qtdSacos;
+    }
+
+    public int getId() { return id; }
+
+    public Grupo getGrupo() { return grupo; }
+
+    public String getNomePessoa() { return nome; }
 }
